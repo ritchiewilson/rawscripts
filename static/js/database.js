a5638579d072496b7ac3a9b46f540cf964dd10d7
Ritchie Wilson <rawilson52@gmail.com>
2010-08-11 18:39:52 -0400

    putzing with db

--- static/js/database.js
+++ static/js/database.js
@@ -5,19 +5,36 @@ var databaseName = 'RawScripts';
 var version = '1.0';
 var displayName = 'RawScripts Local Storage';
 var expectedSize = 1000000;
-
+var username= "rawilson52";
 var database = openDatabase(databaseName, version, displayName, expectedSize);
-
-database.transaction(newUserTable);
-
-
-
-function newUserTable(tx){
-	var SQL = 'CREATE Table '+username+' (id integer, resouce_id TEXT, server_status TEXT, server_timestamp DATETIME, local_timestamp';
-	tx.executeSql(SQL);
+var db = null;
+ 
+try {
+    if (window.openDatabase) {
+        db = openDatabase("NoteTest", "1.0", "HTML5 Database API example", 200000);
+        if (!db)
+            alert("Failed to open the database on disk.  This is probably because the version was bad or there is not enough space left in this domain's quota");
+    } else
+        alert("Couldn't open the database.  Please try with a WebKit nightly with this feature enabled");
+} catch(err) {
+    db = null;
+    alert("Couldn't open the database.  Please try with a WebKit nightly with this feature enabled");
 }
 
 
-function updateUserTable(tx){
-	var SQL = "INSERT INTO "+username
+function loaded()
+{
+    db.transaction(function(tx) {
+        tx.executeSql("SELECT COUNT(*) FROM WebkitStickyNotes", [], function(result) {
+            loadNotes();
+        }, function(tx, error) {
+            tx.executeSql("CREATE TABLE WebKitStickyNotes (id REAL UNIQUE, note TEXT, timestamp REAL, left TEXT, top TEXT, zindex REAL)", [], function(result) { 
+                alert(result); 
+            });
+        });
+    });
 }
+
+db.transaction(function(tx) {
+	tx.executeSql("INSERT INTO WebkitStickyNotes (id, note, timestamp, left, top, zindex) VALUES (?,?,?,?,?,?)",[2,"stuff","stuff","left", "top", "zindex and stuff "]);
+});
