deda70d4d3bf936745f63c10f645b1ab018f6b79
Ritchie Wilson <ritchie@rawscripts.com>
2011-02-18 17:10:00 -0500

    started working on editor js. put sign out back into scriptlist

--- static/js/restricted/editor.js
+++ static/js/restricted/editor.js
@@ -54,11 +54,11 @@ goog.require('goog.editor.plugins.SpacesTabHandler');
 goog.require('goog.editor.plugins.UndoRedo');
 goog.require('goog.ui.editor.DefaultToolbar');
 goog.require('goog.ui.editor.ToolbarController');
-/**
- * @license Rawscripts.com copywrite 2010
- *
- *
- *
+
+/* Closure Compiler for JS changes all function
+ * names. To have human readable function names
+ * in the html, need to attach those functions
+ * to the window object; as done here.
  */
 window['markAsRead'] = markAsRead;
 window['newMessage'] = newMessage;
@@ -194,15 +194,45 @@ var checkSpell=false;
 var fMenu, eMenu, vMenu, sMenu;
 var notesPosition=[];
 
-
+/**
+ * Run on body onload. Checks the brower, 
+ * and the mode the user is running in
+ * and from that alters the DOM and sets
+ * up event listeners.
+ * Then calls for script data
+ */
 function init(){
+	// basicaly, quit stuff if in IE
 	if(goog.userAgent.IE==true){
 		goog.dom.removeNode(goog.dom.getElement('loading'));
 		goog.dom.flattenElement(goog.dom.getElement('canvas'));
 		goog.dom.removeNode(goog.dom.getElement('gtb'));
 		return;
 	}
+	// set up function for resizing windows
+	var vsm = new goog.dom.ViewportSizeMonitor();
+	goog.events.listen(vsm, goog.events.EventType.RESIZE, function(e) {setElementSizes("r");});
+	// then initial resize
+	setElementSizes("i");
+	
+	// sets up key handler
+	var docKh = new goog.events.KeyHandler(document);
+	goog.events.listen(docKh, 'key', keyEvent)
+	
+	// set up mouse click handler
+	goog.events.listen(document, goog.events.EventType.MOUSEMOVE, mouseMove)
+	goog.events.listen(document, goog.events.EventType.MOUSEDOWN, mouseDown)
+	goog.events.listen(document, goog.events.EventType.MOUSEUP, mouseUp)
+	
+	// set up mouse wheel handler
+	var MouseWheelHandler = goog.events.MouseWheelHandler;
+	var MOUSEWHEEL = MouseWheelHandler.EventType.MOUSEWHEEL;
+    var mwh = new MouseWheelHandler(goog.dom.getElement('canvas'));
+	goog.events.listen(mwh, MOUSEWHEEL, handleMouseWheel);
+	goog.events.listen(window, 'unload', function(e){goog.events.unlisten(mwh, MOUSEWHEEL, handleMouseWheel);});
+	
 	if (EOV=='viewer'){
+		// strip dom elements if this is viewer
 		var f = goog.dom.getElement('format');
 		f.style.visibility='hidden'
 		f.disabled=true;
@@ -234,10 +264,10 @@ function init(){
 		goog.dom.removeNode(goog.dom.getElement('revision'));
 		goog.dom.removeNode(goog.dom.getElement('collaborators'));
 		goog.dom.removeNode(goog.dom.getElement('titlePageHref'));
-		
-		
 	}
 	else{
+		// sets up the save button and rename thing
+		// if this is an editor, not viewer
 		var f = goog.dom.getElement('saveButton');
 		f.style.visibility='visible';
 		f.disabled=false;
@@ -259,17 +289,16 @@ function init(){
 			renamePrompt()
 		})
 	}
-	setElementSizes("i");
-	var MouseWheelHandler = goog.events.MouseWheelHandler;
-	var MOUSEWHEEL = MouseWheelHandler.EventType.MOUSEWHEEL;
-    var mwh = new MouseWheelHandler(goog.dom.getElement('canvas'));
-	goog.events.listen(mwh, MOUSEWHEEL, handleMouseWheel);
-	goog.events.listen(window, 'unload', function(e) {
-	goog.events.unlisten(mwh, MOUSEWHEEL, handleMouseWheel);});
+	// Prevent a bunch of defaults
+	// i.e. in some browsers, pressing enter in these
+	// text fields altomaticly opens a new, bad window
 	goog.events.listen(goog.dom.getElement('find_input'), goog.events.EventType.KEYDOWN, function(e){if(e.keyCode==13){e.preventDefault();findDown()}});
 	goog.events.listen(goog.dom.getElement('fr_find_input'), goog.events.EventType.KEYDOWN, function(e){if(e.keyCode==13){e.preventDefault();findDown()}});
 	goog.events.listen(goog.dom.getElement('renameField'), goog.events.EventType.KEYDOWN, function(e){if(e.keyCode==13){e.preventDefault();renameScript()}});
     goog.events.listen(goog.dom.getElement('subject'), goog.events.EventType.KEYDOWN, function(e){if(e.keyCode==13)e.preventDefault()});
+	
+	// if user types in a dom text field,
+	// don't have that show up in the canvas
 	goog.events.listen(goog.dom.getElement('find_input'), goog.events.EventType.FOCUS, function(e){
 		typeToScript=false;
 		findForcePaint=true;
@@ -302,6 +331,10 @@ function init(){
 		findForcePaint=false;
 		commandDownBool=false
 	});
+	
+	// set up menus (file, edit, view, share)
+	// ** needs to be done after DOM elements
+	// stipped because of Editor, or Viewer window
 	fMenu = new goog.ui.Menu();
 	fMenu.decorate(goog.dom.getElement('fileMenu'))
 	fMenu.setPosition(0, 64)
@@ -343,13 +376,30 @@ function init(){
 	goog.events.listen(goog.dom.getElement('share'), goog.events.EventType.MOUSEOUT, topMenuOut);
 	goog.events.listen(sMenu, 'action', menuSelect)
 	
-	
+	// Register Shortcut keys for save, export, undo, redo, find
+	var shortcutHandler = new goog.ui.KeyboardShortcutHandler(document);
+	shortcutHandler.registerShortcut('save', 'meta+s');
+	shortcutHandler.registerShortcut('undo', 'meta+z');
+	shortcutHandler.registerShortcut('redo', 'meta+shift+z');
+	shortcutHandler.registerShortcut('export', 'meta+e');
+	shortcutHandler.registerShortcut('find', 'meta+f');
+	shortcutHandler.setAlwaysPreventDefault(true)
+	goog.events.listen(
+	       shortcutHandler,
+	       goog.ui.KeyboardShortcutHandler.EventType.SHORTCUT_TRIGGERED,
+	       shortcutTriggered
+	);
+	// then update the DOM to reflect shortcut keys on differant OSs
 	var sKeys= [['save','S'],['export', 'E'],['undo', 'Z'], ['redo', 'Shift Z'], ['find', 'F']];
 	var meta = (goog.userAgent.MAC==true ? "⌘" : "Ctrl+")
 	for (i in sKeys){
 		var d = goog.dom.getElement(sKeys[i][0]+'-shortcut');
 		if (d!=null){goog.dom.setTextContent(d, meta+sKeys[i][1]);}
 	}
+	
+	
+	// try to call server for list of contacts
+	// and fill in autocomplete fields
 	try{
 		goog.net.XhrIo.send('/synccontacts',
 			function(e){
@@ -365,13 +415,128 @@ function init(){
 		);
 	}
 	catch(e){};
+	
+	// decorate toolbar
+	var tb = new goog.ui.Toolbar();
+	tb.decorate(goog.dom.getElement('gtb'));
+	goog.events.listen(tb, goog.ui.Component.EventType.ACTION, toolbarActions)
+	goog.dom.getElement('gtb').style.visibility = 'visible';
+	goog.dom.getElement('sidebar').style.visibility = 'visible';
+	
+	// finally, figure out resource_id and call for content
 	resource_id=window.location.href.split('=')[1];
 	goog.net.XhrIo.send('scriptcontent',
-		setup,
+		parseInitialJSON,
 		'POST',
 		'resource_id='+resource_id
 	)
-  }
+}
+
+/**
+ * Gets script data and puts it all in
+ * it's place.
+ * @ param e goog.event.BroswerEvent
+ * JSON from the server with all the script data
+ */
+function parseInitialJSON(e){
+	// if script was not found on server, show that
+    if(e.target.getResponseText()=='not found'){
+        lines = [["Sorry, the script wasn't found.",1]];
+        paint(true,false,false);
+        return;
+    }
+	// else, parse json, put stuff in place
+    var p = e.target.getResponseJson();
+	// p[0] = script title
+	// p[1] = lines of text in the script
+	// p[2] = spelling data
+	// p[3] = notes on the script
+	// p[4] = collaborators on script
+	// p[5] = contacts list
+	// p[6] = autosave setting
+
+	// set up title
+    var title=p[0];
+    goog.dom.getElement('title').innerHTML=title;
+	document.title = title;
+
+	// set up lines of text into global variable
+    var x = p[1];
+    for(var i=0; i<x.length; i++){
+        lines.push([x[i][0], x[i][1]]);
+    }
+	// if this script has just been started
+	// put cursor at the end of line
+    if(lines.length==2){
+        pos.row=anch.row=1;
+        pos.col=anch.col=lines[1][0].length;
+    }
+
+	// put in spelling data into global variable
+    if(p[2].length!=0){
+        var wrong=p[2][0];
+        var ignore =p[2][1];
+        for (w in wrong){
+            spellWrong.push(wrong[w])
+        }
+        for (i in ignore){
+            spellIgnore.push(ignore[i]);
+        }
+    }
+    // put notes into global variable
+    for(i in p[3]){
+        notes.push(p[3][i]);
+    }
+
+	// take collaborators list and put them
+	// into dom
+    var collabs=p[4];
+    var c = goog.dom.getElement('hasAccess');
+    for (i in collabs){
+        var TR = c.appendChild(document.createElement('tr'));
+        TR.id=collabs[i];
+        TR.appendChild(document.createElement('td')).appendChild(document.createTextNode(collabs[i]));
+        var newA = TR.appendChild(document.createElement('td')).appendChild(document.createElement('a'));
+        newA.appendChild(document.createTextNode('Remove Access'));
+        newA.href="javascript:removeAccess('"+collabs[i]+"')";
+		TR=newA=null;
+    }
+
+	// well, shit. This looks redundant. Gotta test this
+	// out and see why this is here. Done on init()
+	var emailAutoComplete = new goog.ui.AutoComplete.Basic(p[5], goog.dom.getElement('recipient'), true);
+	var shareAutoComplete = new goog.ui.AutoComplete.Basic(p[5], goog.dom.getElement('collaborator'), true);
+
+	// changes the autosave bool to user prefrence
+	autosaveBool = (p[6]=='true' ? true : false);
+
+	// open up scene tab
+    tabs(0);
+
+	// figure out character and scene names
+    characterInit();
+    sceneIndex();
+	noteIndex();
+
+	// put actual dom cursor into hidden text box
+    goog.dom.getElement('ccp').focus();
+    goog.dom.getElement('ccp').select();
+
+	// set starting position for save button
+    goog.dom.getElement('saveButton').value="Saved";
+    goog.dom.getElement('saveButton').disabled=true;
+
+	// set up canvas for printing, then print and repeat
+	var canvas = goog.dom.getElement('canvas');
+    var ctx = canvas.getContext('2d');
+    paint(true,false,false);
+    setInterval('paint(false,false,false)', 25);
+
+	// stuff is running, gracefully fade to standard GUI
+	var n = new goog.fx.dom.FadeOutAndHide(goog.dom.getElement('loading'), 500);
+	goog.events.listen(n, goog.fx.Animation.EventType.END, function(e){goog.dom.removeNode(goog.dom.getElement('loading'))})
+	n.play()
+}
 function setElementSizes(v){
 	var s = goog.dom.getViewportSize();
 	goog.style.setSize(goog.dom.getElement('container'), s);
@@ -385,11 +550,7 @@ function setElementSizes(v){
 		paint(false,false,false)
 	}
 }
-var vsm = new goog.dom.ViewportSizeMonitor();
-goog.events.listen(vsm, goog.events.EventType.RESIZE, function(e) {setElementSizes("r");});
 
-var docKh = new goog.events.KeyHandler(document);
-goog.events.listen(docKh, 'key', keyEvent)
 
 function keyEvent(e){
 	if(e.platformModifierKey){
@@ -409,7 +570,6 @@ function keyEvent(e){
       else if(e.keyCode==9){e.preventDefault(); tab();}
 	  else{handlekeypress(e)}
       if(ud<0 && typeToScript && e.keyCode!=13 && e.keyCode!=46){
-		//console.log(ud)
         scroll(ud-400);
       }
 	  if(ud>goog.dom.getElement('canvas').height-80 && typeToScript && e.keyCode!=13 && e.keyCode!=46 && e.keyCode!=8 ){
@@ -426,20 +586,8 @@ function keyEvent(e){
     }
 	goog.dom.getElement('format').selectedIndex=lines[pos.row][1];
 }
-goog.events.listen(document, goog.events.EventType.MOUSEMOVE, mouseMove)
-goog.events.listen(document, goog.events.EventType.MOUSEDOWN, mouseDown)
-goog.events.listen(document, goog.events.EventType.MOUSEUP, mouseUp)
-var shortcutHandler = new goog.ui.KeyboardShortcutHandler(document);
-shortcutHandler.registerShortcut('save', 'meta+s');
-shortcutHandler.registerShortcut('undo', 'meta+z');
-shortcutHandler.registerShortcut('redo', 'meta+shift+z');
-shortcutHandler.registerShortcut('export', 'meta+e');
-shortcutHandler.registerShortcut('find', 'meta+f');
-shortcutHandler.setAlwaysPreventDefault(true)
-goog.events.listen(
-       shortcutHandler,
-       goog.ui.KeyboardShortcutHandler.EventType.SHORTCUT_TRIGGERED,
-       shortcutTriggered);
+
+
 function shortcutTriggered(e){
 	if(e.identifier=="save")save(0);
 	else if(e.identifier=="undo")undo();
@@ -809,73 +957,7 @@ function toolbarActions(e){
 	else if(c=='Spellcheck')launchSpellCheck();
 	else if(c=='Email')emailPrompt();
 }
-function setup(e){
-	var tb = new goog.ui.Toolbar();
-	tb.decorate(goog.dom.getElement('gtb'));
-	goog.events.listen(tb, goog.ui.Component.EventType.ACTION, toolbarActions)
-	goog.dom.getElement('gtb').style.visibility = 'visible';
-	goog.dom.getElement('sidebar').style.visibility = 'visible';
-    if(e.target.getResponseText()=='not found'){
-        lines = [["Sorry, the script wasn't found.",1]];
-        paint(true,false,false);
-        return;
-    }
-    var p = e.target.getResponseJson();
-    var title=p[0];
-    goog.dom.getElement('title').innerHTML=title;
-	document.title = title;
-    var x = p[1];
-    for(var i=0; i<x.length; i++){
-        lines.push([x[i][0], x[i][1]]);
-    }
-    if(lines.length==2){
-        pos.row=anch.row=1;
-        pos.col=anch.col=lines[1][0].length;
-    }
-    if(p[2].length!=0){
-        var wrong=p[2][0];
-        var ignore =p[2][1];
-        for (w in wrong){
-            spellWrong.push(wrong[w])
-        }
-        for (i in ignore){
-            spellIgnore.push(ignore[i]);
-        }
-    }
-    //setupnotes
-    for(i in p[3]){
-        notes.push(p[3][i]);
-    }
-    var collabs=p[4];
-    var c = goog.dom.getElement('hasAccess');
-    for (i in collabs){
-        var TR = c.appendChild(document.createElement('tr'));
-        TR.id=collabs[i];
-        TR.appendChild(document.createElement('td')).appendChild(document.createTextNode(collabs[i]));
-        var newA = TR.appendChild(document.createElement('td')).appendChild(document.createElement('a'));
-        newA.appendChild(document.createTextNode('Remove Access'));
-        newA.href="javascript:removeAccess('"+collabs[i]+"')";
-		TR=newA=null;
-    }
-	var emailAutoComplete = new goog.ui.AutoComplete.Basic(p[5], goog.dom.getElement('recipient'), true);
-	var shareAutoComplete = new goog.ui.AutoComplete.Basic(p[5], goog.dom.getElement('collaborator'), true);
-	autosaveBool = (p[6]=='true' ? true : false);
-    var canvas = goog.dom.getElement('canvas');
-    var ctx = canvas.getContext('2d');
-    tabs(0);
-    characterInit();
-    sceneIndex();
-    goog.dom.getElement('ccp').focus();
-    goog.dom.getElement('ccp').select();
-    goog.dom.getElement('saveButton').value="Saved";
-    goog.dom.getElement('saveButton').disabled=true;
-    paint(true,false,false);
-    setInterval('paint(false,false,false)', 25);
-	var n = new goog.fx.dom.FadeOutAndHide(goog.dom.getElement('loading'), 500);
-	goog.events.listen(n, goog.fx.Animation.EventType.END, function(e){goog.dom.removeNode(goog.dom.getElement('loading'))})
-	n.play()
-	noteIndex();
-}
+
 function tabs(v){
     var t = ["sceneTab","noteTab"]
     for(i in t){
