ef95d77efeb2857eb53e200cae322a21a1a778f9
ritchie <ritchie@ritchie-Pangolin-Performance.(none)>
2011-08-22 11:20:55 -0400

    fixed bash scripts to they can be called from anywhere

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+
+#Get the root directory of the project
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR
+cd ..
+ROOT=$(pwd)
+
+
 #Closure Compiler
+cd $ROOT"/scripts"
 mkdir compiler && cd compiler
 wget http://closure-compiler.googlecode.com/files/compiler-latest.zip
 unzip *.zip
 rm *.zip
-cd ..
 
 # Closure library
-cd ..
-cd static
+cd $ROOT"/static"
 svn checkout http://closure-library.googlecode.com/svn/trunk/ closure-library
 
 #Gdata
-cd ..
+cd $ROOT
 wget http://gdata-python-client.googlecode.com/files/gdata-2.0.14.tar.gz
 tar xvzf gdata-2.0.14.tar.gz
 mv gdata-2.0.14/src/gdata gdata
