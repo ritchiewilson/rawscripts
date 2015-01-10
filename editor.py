130551d18634edb1430ec1557406dd5fc68daf4b
ritchiew <rawilson52@gmail.com>
2015-01-10 15:54:43 -0500

    basic style cleanups in editor.py

--- editor.py
+++ editor.py
@@ -24,17 +24,11 @@ import os
 import cgi
 import wsgiref.handlers
 from google.appengine.api import users
-from google.appengine.api import mail
-from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 import datetime
-import random
-import zipfile
-import export
-import logging
 from django.utils import simplejson
 import mobileTest
 from google.appengine.api import memcache
@@ -57,17 +51,14 @@ class Editor (webapp.RequestHandler):
         user = users.get_current_user()
         user_string = gcu()
         path = os.path.join(os.path.dirname(__file__), 'html/editor.html')
-        template_values = {}
-        template_values['EOV'] = "editor"
-        resource_id=self.request.get('resource_id')
-        logging.info(user_string)
-        logging.info(resource_id)
-        format='editor'
+        template_values = {'EOV': "editor"}
+        resource_id = self.request.get('resource_id')
+        format = 'editor'
         mobile = mobileTest.mobileTest(self.request.user_agent)
         if mobile == 1:
             self.redirect('/scriptlist')
             return;
-        if user and resource_id!="Demo":
+        if user and resource_id != "Demo":
             template_values['sign_out'] = users.create_logout_url('/')
             template_values['user'] = users.get_current_user().email()
             r = db.get(db.Key.from_path('UsersScripts', 'owner'+gcu()+resource_id))
@@ -77,7 +68,7 @@ class Editor (webapp.RequestHandler):
                     format='viewer'
                     path = os.path.join(os.path.dirname(__file__), 'html/editor.html')
                     template_values['EOV'] = "viewer"
-                    q=db.GqlQuery("SELECT * FROM ShareNotify "+
+                    q = db.GqlQuery("SELECT * FROM ShareNotify "+
                                     "WHERE user='"+user_string+"' "+
                                     "AND resource_id='"+resource_id+"' "+
                                     "AND opened=False")
@@ -87,12 +78,10 @@ class Editor (webapp.RequestHandler):
                         unopened[0].timeopened = datetime.datetime.today()
                         unopened[0].put()
             if not r:
-                logging.info("being redirected")
                 self.redirect("/")
                 return
         else:
-            resource_id=self.request.get('resource_id')
-            if resource_id=='Demo':
+            if resource_id == 'Demo':
                 template_values['sign_out'] =  '/'
                 template_values['user'] = "test@example.com"
             else:
@@ -100,14 +89,17 @@ class Editor (webapp.RequestHandler):
                                     'yahoo_sign_in' : users.create_login_url('/editor?resource_id='+resource_id, None, 'yahoo.com')}
                 path = os.path.join(os.path.dirname(__file__), 'html/login.html')
 
-        if user:
-            user=gcu()
-        else:
-            user="unknown"
 
-        dev_js = ['base', 'calc', 'canvas-manipulate', 'ccp', 'dom-manipulate', 'draw', 'editor', 'findAndReplace', 'init', 'keyboard',    'menu',    'mouse',    'notes', 'prompts', 'sort', 'spellcheck', 'window']
+        dev_js = ['base', 'calc', 'canvas-manipulate', 'ccp', 'dom-manipulate',
+                  'draw', 'editor', 'findAndReplace', 'init', 'keyboard',
+                  'menu', 'mouse', 'notes', 'prompts', 'sort', 'spellcheck',
+                  'window']
         pro_js = ['editor-compiled']
-        dev_css = ['menu', 'menuitem','menuseparator','common','toolbar','button','custombutton','autocomplete', 'dialog', 'linkbutton', 'tab', 'tabbar', 'colormenubutton', 'palette',    'colorpalette',    'editor/bubble', 'editor/dialog', 'editor/linkdialog', 'editortoolbar']
+        dev_css = ['menu', 'menuitem', 'menuseparator', 'common', 'toolbar',
+                   'button', 'custombutton', 'autocomplete', 'dialog',
+                   'linkbutton', 'tab', 'tabbar', 'colormenubutton', 'palette',
+                   'colorpalette', 'editor/bubble', 'editor/dialog',
+                   'editor/linkdialog', 'editortoolbar']
         pro_css = []
         template_values['resource_id'] = resource_id
         template_values['MODE'] = config.MODE
@@ -123,115 +115,118 @@ class ScriptContent (webapp.RequestHandler):
         resource_id = self.request.get('resource_id')
 
         q = db.GqlQuery("SELECT * FROM UsersScripts "+
-                                        "WHERE resource_id='"+resource_id+"'")
+                        "WHERE resource_id='"+resource_id+"'")
         results = q.fetch(500)
-        p=False
         if len(results)==0:
             self.response.headers["Content-Type"]='text/plain'
             self.response.out.write('not found')
+        p = False
         if resource_id=='Demo':
-            p=True
-            title="Duck Soup"
+            p = True
+            title = "Duck Soup"
         else:
             for i in results:
-                title=i.title
-                if i.user==user or users.is_current_user_admin():
-                    if i.permission=='owner' or i.permission=="collab" or users.is_current_user_admin():
-                        p=True
+                title = i.title
+                if i.user == user or users.is_current_user_admin():
+                    if i.permission == 'owner' or i.permission == "collab" or users.is_current_user_admin():
+                        p = True
 
-        if p==True:
-            q = db.GqlQuery("SELECT * FROM ScriptData "+
-                                            "WHERE resource_id='"+resource_id+"' "+
-                                            "ORDER BY version DESC")
-            results = q.get()
+        if not p:
+            self.response.headers["Content-Type"]='text/plain'
+            self.response.out.write('not found')
+            return
+        q = db.GqlQuery("SELECT * FROM ScriptData "+
+                        "WHERE resource_id='"+resource_id+"' "+
+                        "ORDER BY version DESC")
+        results = q.get()
 
-            q = db.GqlQuery("SELECT * FROM SpellingData "+
-                                            "WHERE resource_id='"+resource_id+"'")
-            spellresults = q.fetch(2)
-            sp = []
-            if len(spellresults)!=0:
-                sp.append(simplejson.loads(spellresults[0].wrong))
-                sp.append(simplejson.loads(spellresults[0].ignore))
+        q = db.GqlQuery("SELECT * FROM SpellingData "+
+                        "WHERE resource_id='"+resource_id+"'")
+        spellresults = q.fetch(2)
+        sp = []
+        if len(spellresults)!=0:
+            sp.append(simplejson.loads(spellresults[0].wrong))
+            sp.append(simplejson.loads(spellresults[0].ignore))
 
-            q=db.GqlQuery("SELECT * FROM Notes "+
-                                        "WHERE resource_id='"+resource_id+"'")
-            noteresults = q.fetch(1000)
-            user = users.get_current_user()
-            if user:
-                q=db.GqlQuery("SELECT * FROM UnreadNotes "+
-                            "WHERE resource_id='"+resource_id+"' "+
-                            "AND user='"+gcu()+"'")
-                un=q.fetch(500)
+        q = db.GqlQuery("SELECT * FROM Notes "+
+                        "WHERE resource_id='"+resource_id+"'")
+        noteresults = q.fetch(1000)
+        user = users.get_current_user()
+        if user:
+            q=db.GqlQuery("SELECT * FROM UnreadNotes "+
+                        "WHERE resource_id='"+resource_id+"' "+
+                        "AND user='"+gcu()+"'")
+            un=q.fetch(500)
+        else:
+            un=None
+        notes=[]
+        for i in noteresults:
+            msgs = simplejson.loads(i.data)
+            if un==None:
+                for unit in msgs:
+                    unit.append(1)
             else:
-                un=None
-            notes=[]
-            for i in noteresults:
-                msgs = simplejson.loads(i.data)
-                if un==None:
-                    for unit in msgs:
+                for unit in msgs:
+                    found=False
+                    for j in un:
+                        if j.msg_id==unit[2]:
+                            unit.append(0)
+                            found=True
+                    if found==False:
                         unit.append(1)
-                else:
-                    for unit in msgs:
-                        found=False
-                        for j in un:
-                            if j.msg_id==unit[2]:
-                                unit.append(0)
-                                found=True
-                        if found==False:
-                            unit.append(1)
 
-                msgsArr=[]
-                for j in msgs:
-                    msgsArr.append({'text':j[0], 'user':j[1], 'msg_id':j[2], 'readBool':j[3]})
+            msgsArr=[]
+            for j in msgs:
+                msgsArr.append({'text':j[0], 'user':j[1], 'msg_id':j[2], 'readBool':j[3]})
 
-                dic = { 'row':i.row,
-                        'col':i.col,
-                        'msgs':msgsArr,
-                        'thread_id':i.thread_id}
-                notes.append(dic)
+            dic = { 'row':i.row,
+                    'col':i.col,
+                    'msgs':msgsArr,
+                    'thread_id':i.thread_id}
+            notes.append(dic)
 
-            sharedwith=[]
-            q=db.GqlQuery("SELECT * FROM UsersScripts "+
-                                        "WHERE resource_id='"+resource_id+"'")
-            shareresults=q.fetch(50)
-            for i in shareresults:
-                if i.permission=="collab":
-                    sharedwith.append(i.user)
+        sharedwith=[]
+        q=db.GqlQuery("SELECT * FROM UsersScripts "+
+                                    "WHERE resource_id='"+resource_id+"'")
+        shareresults=q.fetch(50)
+        for i in shareresults:
+            if i.permission=="collab":
+                sharedwith.append(i.user)
 
-            try:
-                c = memcache.get('contacts' + gcu())
-            except:
-                c=None
-            if c==None:
-                contacts = []
-            else:
-                contacts = simplejson.loads(c)
+        try:
+            c = memcache.get('contacts' + gcu())
+        except:
+            c=None
+        if c==None:
+            contacts = []
+        else:
+            contacts = simplejson.loads(c)
 
-            try:
-                us = db.get(db.Key.from_path('UsersSettings', 'settings'+gcu()))
-            except:
-                us = None
-            if us==None:
+        try:
+            us = db.get(db.Key.from_path('UsersSettings', 'settings'+gcu()))
+        except:
+            us = None
+        if us==None:
+            autosave='true'
+        else:
+            if us.autosave==True:
                 autosave='true'
             else:
-                if us.autosave==True:
-                    autosave='true'
-                else:
-                    autosave='false'
+                autosave='false'
 
-            ja={}
-            ja['title'] = title
-            ja['lines'] = simplejson.loads(results.data)
-            ja['spelling'] = sp
-            ja['notes'] = notes
-            ja['sharedwith'] = sharedwith
-            ja['contacts'] = contacts
-            ja['autosave'] = autosave
+        ja={}
+        ja['title'] = title
+        ja['lines'] = simplejson.loads(results.data)
+        ja['spelling'] = sp
+        ja['notes'] = notes
+        ja['sharedwith'] = sharedwith
+        ja['contacts'] = contacts
+        ja['autosave'] = autosave
 
-            content = simplejson.dumps(ja)
+        content = simplejson.dumps(ja)
 
-            self.response.headers["Content-Type"]='text/plain'
-            self.response.out.write(content)
+        self.response.headers["Content-Type"]='text/plain'
+        self.response.out.write(content)
 
 
 class Save (webapp.RequestHandler):
@@ -239,58 +234,52 @@ class Save (webapp.RequestHandler):
         resource_id = self.request.get('resource_id')
         if resource_id == None:
             return
-        if resource_id=='Demo':
+        if resource_id == 'Demo':
             self.response.out.write('demo')
             return
-        data=self.request.get('data')
-        autosave = self.request.get('autosave')
-        if autosave=='0':
-            b=0
-        else:
-            b=1
+        data = self.request.get('data')
+        autosave = 0 if self.request.get('autosave') == "0" else 1
         q = db.GqlQuery("SELECT * FROM UsersScripts "+
-                                        "WHERE resource_id='"+resource_id+"' "+
-                                        "AND permission='owner'")
+                        "WHERE resource_id='"+resource_id+"' "+
+                        "AND permission='owner'")
         u = q.get()
-        v=0
-        if u==None:
+        if u == None:
             self.response.headers["Content-Type"] = "text/plain"
             self.response.out.write('script not found')
-            logging.info('script not found')
             return
-        else:
-            if u.user==gcu():
-                v = memcache.get('version'+resource_id)
-                if v==None:
-                    q = db.GqlQuery("SELECT * FROM ScriptData "+
-                            "WHERE resource_id='"+resource_id+"' "+
-                            "ORDER BY version DESC")
-                    most_recent = q.get()
-                    v = most_recent.version
-                    v+=1
-                    memcache.set('version'+resource_id, v)
-                else:
-                    v+=1
-                    memcache.set('version'+resource_id, v)
-        if not v==0:
-            a = models.ScriptData(resource_id=resource_id,
-                            title='title',
-                            data=data,
-                            version=v,
-                            export='[[],[]]',
-                            tag='',
-                            autosave=b)
-            a.put()
 
+        v = 0 # latest version number. I think it also enforces permissions...
+        if u.user == gcu():
+            v = memcache.get('version'+resource_id)
+            if v == None:
+                q = db.GqlQuery("SELECT * FROM ScriptData "+
+                                "WHERE resource_id='"+resource_id+"' "+
+                                "ORDER BY version DESC")
+                most_recent = q.get()
+                v = most_recent.version
+            v += 1
+            memcache.set('version'+resource_id, v)
 
-            u.last_updated=datetime.datetime.today()
-            u.put()
-
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('1')
-        else:
+        if v == 0:
             self.response.headers['Content-Type'] = 'text/plain'
             self.response.out.write('0')
+            return
+
+        a = models.ScriptData(resource_id=resource_id,
+                        title='title',
+                        data=data,
+                        version=v,
+                        export='[[],[]]',
+                        tag='',
+                        autosave=autosave)
+        a.put()
+
+
+        u.last_updated=datetime.datetime.today()
+        u.put()
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('1')
 
 
 def main():
