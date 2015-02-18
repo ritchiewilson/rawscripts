2a2ceb5a90804e7f132667688af029336ebf2efd
ritchiew <rawilson52@gmail.com>
2015-02-18 10:31:57 -0500

    more scripts for migrating to new db format

--- flask_models.py
+++ flask_models.py
@@ -1,3 +1,7 @@
+import difflib
+import json
+from datetime import datetime
+
 from rawscripts import db
 
 class User(db.Model):
@@ -41,18 +45,30 @@ class ScriptData(db.Model):
                      db.Index('ix_script_data_timestamp', 'timestamp'))
 
     @staticmethod
+    def get_latest_version(resource_id):
+        latest = ScriptData.query.filter_by(resource_id=resource_id). \
+                     order_by(db.desc('version')).first()
+        return latest
+
+    @staticmethod
     def thin_raw_data(resource_id):
         check = MigrationCheck.query.filter_by(resource_id=resource_id).first()
         if not check:
             return
-        last_version = check.verified_to - 3
+        # don't delete first save if duplicated from another script
+        from_version = -1
+        dup = DuplicateScript.query.filter_by(new_script=resource_id).first()
+        if dup:
+            from_version = dup.from_version
+        last_version = check.verified_to
         data = ScriptData.query.filter_by(resource_id=resource_id). \
+                   filter(ScriptData.version < last_version). \
                    with_entities(ScriptData.tag, ScriptData.export, ScriptData.version). \
                    all()
         versions_to_delete = []
         for d in data:
             if d.tag == '' and d.export == '[[],[]]' and d.version % 250 != 0:
-                if d.version < last_version:
+                if d.version != from_version + 1:
                     versions_to_delete.append(d.version)
         if versions_to_delete:
             ScriptData.query.filter_by(resource_id=resource_id). \
@@ -60,6 +76,172 @@ class ScriptData(db.Model):
                 delete(synchronize_session=False)
         db.session.commit()
         db.session.expire_all()
+
+    @staticmethod
+    def get_content_for_version(resource_id, version):
+        if version == 0:
+            return ''
+        # get most recent full save
+        prev_string = ''
+        prev_version = 0
+        full_save = ScriptData.query.filter_by(resource_id=resource_id). \
+                        filter(ScriptData.version <= version). \
+                        order_by(db.desc('version')).first()
+        if full_save:
+            prev_string = full_save.data
+            prev_version = full_save.version
+        elif DuplicateScript.has_parent(resource_id):
+            dup = DuplicateScript.query. \
+                      filter_by(new_script=resource_id).first()
+            if dup.from_version <= version:
+                prev_string = ScriptData.get_content_for_version(dup.from_script,
+                                                                 dup.from_version)
+                prev_version = dup.from_version
+            else:
+                return ScriptData.get_content_for_version(dup.from_script, version)
+
+        saves = ResourceVersion.query.filter_by(resource_id=resource_id).\
+                    filter(ResourceVersion.version > prev_version). \
+                    filter(ResourceVersion.version <= version). \
+                    order_by('version').options(db.subqueryload('ops')).all()
+        for save in saves:
+            prev_string = ScriptData.apply_ops(prev_string, save.ops)
+        return prev_string
+
+    @staticmethod
+    def verify_screenplay_at_version(resource_id, version):
+        prev = ScriptData.get_content_for_version(resource_id, version - 1)
+        ops = ResourceVersion.query.filter_by(resource_id=resource_id,
+                                              version=version).first().ops
+        new_string = ScriptData.apply_ops(prev, ops)
+        expected = ScriptData.get_content_for_version(resource_id, version)
+        return new_string == expected
+
+    @staticmethod
+    def apply_ops(string, ops):
+        _ops = sorted(ops, key=lambda op: op.application_index)
+        for op in _ops:
+            tag = ["insert", "delete", "replace"][op.action]
+            if tag == "delete":
+                string = string[:op.offset] + string[op.offset + op.amount:]
+            if tag == "insert":
+                string = string[:op.offset] + op.text + string[op.offset:]
+            if tag == "replace":
+                string = string[:op.offset] + op.text + string[op.offset + op.amount:]
+        return string
+
+    @staticmethod
+    def has_duplicate_versions(resource_id, start_from, end_at):
+        saves = ScriptData.query.filter_by(resource_id=resource_id). \
+                    filter(ScriptData.version >= start_from). \
+                    filter(ScriptData.version <= end_at). \
+                    order_by('version').with_entities(ScriptData.version).all()
+        for save in saves:
+            if save.version != start_from:
+                return True
+            start_from += 1
+        return False
+
+    @staticmethod
+    def migrate_version(resource_id, version):
+        # check if this version was already migrated
+        exists = ResourceVersion.query. \
+                     filter_by(resource_id=resource_id, version=version).first()
+        if exists:
+            return
+        this_version = ScriptData.query.filter_by(resource_id=resource_id,
+                                                  version=version).all()
+        if len(this_version) == 0:
+            raise Exception('No saved raw data for this version:',
+                            resource_id, version)
+        if len(this_version) > 1:
+            raise Exception('Multiple saved raw data for this version:',
+                            resource_id, version)
+        this_version = this_version[0]
+        rv = ResourceVersion(resource_id=resource_id,
+                             version=version,
+                             timestamp=this_version.timestamp,
+                             autosave=bool(this_version.autosave))
+        prev_string = ScriptData.get_content_for_version(resource_id, version-1)
+        new_string = this_version.data
+        opcodes = ScriptData.get_opcodes(prev_string, new_string)
+        application_index = 0
+        for tag, i1, i2, j1, j2 in reversed(opcodes):
+            if tag == "equal":
+                continue
+            op = Op(offset=i1, application_index=application_index)
+            op.action = ["insert", "delete", "replace"].index(tag)
+            if tag == "delete":
+                op.amount = (i2 - i1)
+            elif tag == "insert":
+                op.text = new_string[j1:j2]
+            elif tag == "replace":
+                op.amount = (i2 - i1)
+                op.text = new_string[j1:j2]
+            rv.ops.append(op)
+            application_index += 1
+        ScriptData.save_tags(this_version, rv)
+        db.session.add(rv)
+        db.session.commit()
+
+    @staticmethod
+    def save_tags(snapshot, resource_version):
+        # save all the email, export, and user defined tags
+        if snapshot.tag is not None and snapshot.tag != '':
+            tag = VersionTag(_type='user',
+                             value=snapshot.tag,
+                             timestamp=resource_version.timestamp)
+            resource_version.tags.append(tag)
+
+        def save_tag(entry, _type):
+            value = entry[0]
+            timestamp = datetime.strptime(entry[1], "%Y-%m-%d %H:%M:%S.%f")
+            tag = VersionTag(_type=_type,
+                             value=value,
+                             timestamp=timestamp)
+            resource_version.tags.append(tag)
+        emails, exports = json.loads(snapshot.export)
+        for email in emails:
+            save_tag(email, 'email')
+        for export in exports:
+            save_tag(export, 'export')
+
+    @staticmethod
+    def get_diff_strings_and_offset(string0, string1):
+        offset = 0
+        diff_string0, diff_string1 = string0, string1
+
+        while diff_string0 and diff_string1 and \
+              diff_string0[:1000] == diff_string1[:1000]:
+            diff_string0 = diff_string0[990:]
+            diff_string1 = diff_string1[990:]
+            offset += 990
+
+        while diff_string0 and diff_string1 and \
+              diff_string0[-1000:] == diff_string1[-1000:]:
+            diff_string0 = diff_string0[:-990]
+            diff_string1 = diff_string1[:-990]
+
+        return diff_string0, diff_string1, offset
+
+    @staticmethod
+    def get_opcodes(string0, string1):
+        if string0 == string1:
+            return []
+        diff_string0, diff_string1, offset = \
+            ScriptData.get_diff_strings_and_offset(string0, string1)
+
+        diff = difflib.SequenceMatcher(None, diff_string0, diff_string1)
+        opcodes = diff.get_opcodes()
+        # add offset in case we only needed to diff second half of texts
+        opcodes = [(tag, i1 + offset, i2 + offset, j1 + offset, j2 + offset) for
+                   tag, i1, i2, j1, j2 in opcodes]
+        # only the first opcode should have changed. make sure they start from 0
+        if opcodes[0][0] == 'equal':
+            f = opcodes[0]
+            opcodes[0] = (f[0], 0, f[2], 0, f[4])
+        return opcodes
+
 
     def __repr__(self):
        return "<ScriptData(resource_id='%s', version='%s')>" % (self.resource_id, str(self.version))
@@ -77,6 +259,11 @@ class ResourceVersion(db.Model):
 
     __table_args__= (db.Index('ix_resource_versions_resource_id_version', "resource_id", db.asc('version')),)
 
+    @staticmethod
+    def get_latest_version(resource_id):
+        latest = ResourceVersion.query.filter_by(resource_id=resource_id). \
+                     order_by(db.desc('version')).first()
+        return latest
 
 class Op(db.Model):
     __tablename__ = "ops"
@@ -143,3 +330,9 @@ class DuplicateScript(db.Model):
     new_script = db.Column(db.String)
     from_version = db.Column(db.Integer)
     __key__ = db.Column(db.String)
+
+    @staticmethod
+    def has_parent(resource_id):
+        is_dup = DuplicateScript.query. \
+                     filter_by(new_script=resource_id).first()
+        return is_dup is not None
--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask.ext.script import Manager
 
 from rawscripts import app, db
@@ -5,13 +7,100 @@ from flask_models import *
 
 manager = Manager(app)
 
+def get_resource_ids():
+    # resource_ids = UsersScripts.get_all_resource_ids()
+    current = datetime.utcnow()
+    days_ago = current.replace(day=13)
+    stuff = UsersScripts.query.filter(UsersScripts.last_updated > days_ago). \
+            order_by('resource_id').all()
+    resource_ids = [s.resource_id for s in stuff]
+    return resource_ids
 
 @manager.command
 def thin_screenplays():
-    resource_ids = UsersScripts.get_all_resource_ids()
+    # resource_ids = UsersScripts.get_all_resource_ids()
+    resource_ids = get_resource_ids()
     for n, resource_id in enumerate(resource_ids):
         ScriptData.thin_raw_data(resource_id)
         print "did screenplay:", n, resource_id
+    print "Done"
+
+def verify_screenplay(resource_id):
+    if resource_id == 'Demo':
+        return
+    start_from = 1
+    check = MigrationCheck.query.filter_by(resource_id=resource_id).first()
+    if check:
+        start_from = check.verified_to
+    elif DuplicateScript.has_parent(resource_id):
+        dup = DuplicateScript.query.filter_by(new_script=resource_id).first()
+        start_from = max(start_from, dup.from_version + 1)
+    last_save = ResourceVersion.get_latest_version(resource_id)
+    if not last_save:
+        print "Not migrated:", resource_id
+        return
+    end_at = last_save.version
+    if ScriptData.has_duplicate_versions(resource_id, start_from, end_at):
+        print "Script has multiple saves with same version number:", resource_id
+        return
+
+    last_verified = None
+
+    for version in range(start_from, end_at + 1):
+        if ScriptData.verify_screenplay_at_version(resource_id, version):
+            last_verified = version
+        else:
+            print "Error checking version:", resource_id, version
+            return
+    print "Last verified was:", resource_id, last_verified
+    if last_verified is None:
+        return
+    check = MigrationCheck.query.filter_by(resource_id=resource_id).first()
+    if not check:
+        check = MigrationCheck(resource_id=resource_id)
+        db.session.add(check)
+    check.verified_to = last_verified
+    db.session.commit()
+    if start_from != last_verified:
+        print "Moved from", start_from, "to", last_verified
+
+@manager.command
+def verify_screenplays():
+    # resource_ids = UsersScripts.get_all_resource_ids()
+    resource_ids = get_resource_ids()
+    for resource_id in resource_ids:
+        verify_screenplay(resource_id)
+    print "Done"
+
+def migrate_screenplay(resource_id):
+    if resource_id == 'Demo':
+        return
+    latest_raw = ScriptData.get_latest_version(resource_id)
+    latest_migrated = ResourceVersion.get_latest_version(resource_id)
+    if latest_migrated and latest_raw.version == latest_migrated.version:
+        print "Already fully migrated:", resource_id
+        return
+    start_from = 1
+    if latest_migrated:
+        start_from = latest_migrated.version + 1
+    else:
+        first_raw = ScriptData.query.filter_by(resource_id=resource_id). \
+                        order_by(db.asc('version')).first()
+        if first_raw.version != 1 and not DuplicateScript.has_parent(resource_id):
+            raise Exception('Screenplay has no first version, but not dup', resource_id)
+        start_from = first_raw.version
+    end_at = latest_raw.version
+    for version in range(start_from, end_at + 1):
+        ScriptData.migrate_version(resource_id, version)
+    print "Migrated from version", start_from, "to", end_at
+
+@manager.command
+def migrate_to_ops():
+    # resource_ids = UsersScripts.get_all_resource_ids()
+    resource_ids = get_resource_ids()
+    print "Screenplays to check", len(resource_ids)
+    for resource_id in resource_ids:
+        migrate_screenplay(resource_id)
     print "Done"
 
 if __name__ == "__main__":
