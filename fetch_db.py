18ccaa9ed1058ee6b72ede36860dbc27d46ddd27
ritchiew <rawilson52@gmail.com>
2015-03-10 13:29:22 -0400

    huge, ugly db fetch script and its dependencies

---
+++ fetch_db.py
@@ -0,0 +1,162 @@
+# Rawscripts - Screenwriting Software
+# Copyright (C) Ritchie Wilson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+import StringIO
+import os
+import cgi
+import string
+from datetime import datetime
+import wsgiref.handlers
+from google.appengine.api import users
+from google.appengine.api import memcache
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+from django.utils import simplejson
+import config
+import models
+import random
+try:
+    from Crypto.Cipher import AES
+except:
+    pass
+
+class FetchPass(webapp.RequestHandler):
+    def get(self):
+        if not users.is_current_user_admin():
+            return
+        chars = string.uppercase + string.lowercase + string.digits
+        def get_random_chars(n):
+            return ''.join([random.choice(chars) for x in range(n)])
+        password = get_random_chars(24)
+        iv = get_random_chars(16)
+        memcache.set(key='fetch_password', value=password)
+        memcache.set(key='fetch_iv', value=iv)
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(password + " " + iv)
+
+
+class FetchDB(webapp.RequestHandler):
+    def get(self):
+        password = memcache.get('fetch_password')
+        iv = memcache.get('fetch_iv')
+        if password is None or iv is None:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write("Missing password or IV")
+            return
+
+        table = self.request.get('table')
+        output = unicode('@')
+        if table == "Users":
+            output += self.fetch_by_timestamps('firstUse', models.Users,
+                                               self.user_to_string)
+        if table == "OpenID2":
+            output += self.fetch_by_timestamps('timestamp', models.OpenIDData2,
+                                               self.open_id2_to_string)
+        if table == "ScriptData":
+            output += self.fetch_by_timestamps('timestamp', models.ScriptData,
+                                               self.script_data_to_string, do_json=True)
+        if table == "DuplicateScripts":
+            output += self.fetch_duplicate_scripts()
+
+        if table == "Folders":
+            output += self.fetch_folders()
+
+        if table == "UsersScripts":
+            output += self.fetch_by_timestamps('last_updated', models.UsersScripts,
+                                               self.users_scripts_to_string, do_json=True)
+
+        diff = 16 - (len(output) % 16)
+        output = ('!' * diff) + output
+        obj = AES.new(password, AES.MODE_CBC, iv)
+        ciphertext = obj.encrypt(output)
+        import base64
+        ciphertext = base64.b64encode(ciphertext)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(ciphertext)
+
+    def user_to_string(self, user):
+        fields = [user.name, user.firstUse]
+        fields = [str(field) for field in fields]
+        return ','.join(fields)
+
+    def open_id2_to_string(self, user):
+        fields = [user.nickname, user.email, user.user_id,
+                  user.federated_identity, user.federated_provider,
+                  user.timestamp]
+        fields = [str(field) for field in fields]
+        return ','.join(fields)
+
+    def script_data_to_string(self, script_data):
+        d = [script_data.resource_id, script_data.data, script_data.version,
+             str(script_data.timestamp), script_data.autosave,
+             script_data.export, script_data.tag]
+        return d
+
+    def users_scripts_to_string(self, script):
+        d = [script.user, script.resource_id, script.title,
+             str(script.last_updated), script.permission, script.folder]
+        return d
+
+    def fetch_by_timestamps(self, timestamp_field, model, str_func, do_json=False):
+        limit = int(self.request.get('limit'))
+        start_time = self.request.get('start_time')
+        dt, microseconds = start_time.split('.')
+        timestamp = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
+        timestamp = timestamp.replace(microsecond=int(microseconds))
+        query = model.all()
+        query.order(timestamp_field)
+        query.filter(timestamp_field + ' >', timestamp)
+        if do_json:
+            data = [str_func(row) for row in query.run(limit=limit)]
+            return simplejson.dumps(data)
+        output = ''
+        for row in query.run(limit=limit):
+            output += str_func(row) + '\n'
+        return output
+
+    def fetch_duplicate_scripts(self):
+        query = models.DuplicateScripts.all()
+        output = ''
+        for row in query.run():
+            vals = [str(row.key()), row.from_script,
+                    row.new_script, str(row.from_version)]
+            output += ','.join(vals) + '\n'
+        return output
+
+    def fetch_folders(self):
+        query = models.Folders.all()
+        output = []
+        for row in query.run():
+            vals = [str(row.key()), row.user, row.data]
+            output.append(vals)
+        return simplejson.dumps(output)
+
+def main():
+    application = webapp.WSGIApplication([('/fetchdb', FetchDB),
+                                          ('/fetchpass', FetchPass),],
+                                         debug=True)
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
---
+++ fetch_script.py
@@ -0,0 +1,200 @@
+from Crypto.Cipher import AES
+import requests
+import base64
+import json
+import time
+import sys
+from datetime import datetime
+from sqlalchemy import create_engine, desc
+from sql_models import Base, User, OpenIDData2, ScriptData, UsersScripts, \
+    DuplicateScript, Folder
+engine = create_engine('sqlite:///rawscripts.db', echo=False)
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+
+# Base.metadata.create_all(engine)
+URL = 'http://www.rawscripts.com/fetchdb'
+START_TIME = None
+
+if len(sys.argv) != 3:
+    raise Exception('Need to give password and IV to fetch files.')
+
+PASSWORD = sys.argv[1]
+IV = sys.argv[2]
+
+def fetch(params):
+    r = requests.get(URL, params=params)
+    text = r.text
+    if text.startswith('Missing'):
+        raise Exception(text)
+    ciphertext = base64.b64decode(r.text)
+    obj = AES.new(PASSWORD, AES.MODE_CBC, IV)
+    plaintext = obj.decrypt(ciphertext)
+    return plaintext[plaintext.index('@') + 1:]
+
+
+def commit_users(data, session):
+    last_time = None
+    for line in data.split('\n'):
+        if len(line) == 0:
+            continue
+        email, timestamp = line.split(',')
+        firstUse = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+        user = User(name=email, firstUse=firstUse)
+        session.add(user)
+        last_time = firstUse
+    session.commit()
+    return last_time
+
+def commit_open_id_data2(data, session):
+    last_time = None
+    for line in data.split('\n'):
+        if len(line) == 0:
+            continue
+        nickname, email, user_id, federated_identity, federated_provider, timestamp = line.split(',')
+        timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+        open_id = OpenIDData2(nickname=nickname,
+                              email=email,
+                              user_id=user_id,
+                              federated_identity=federated_identity,
+                              federated_provider=federated_provider,
+                              timestamp=timestamp)
+        session.add(open_id)
+        last_time = timestamp
+    session.commit()
+    return last_time
+
+def commit_script_data(data, session):
+    last_time = None
+    lines = json.loads(data)
+    ids = {}
+    for resource_id, script_data, version, timestamp, autosave, export, tag in lines:
+        version = int(version)
+        timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+        autosave = bool(int(autosave))
+        obj = ScriptData(resource_id=resource_id,
+                         data=script_data,
+                         version=version,
+                         timestamp=timestamp,
+                         autosave=autosave,
+                         export=export,
+                         tag=tag)
+        session.add(obj)
+        last_time = timestamp
+        ids[resource_id] = True
+
+    session.commit()
+    return last_time
+
+def commit_users_scripts(data, session):
+    last_time = None
+    lines = json.loads(data)
+    for user, resource_id, title, timestamp, permission, folder in lines:
+        timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+        row = session.query(UsersScripts).filter_by(resource_id=resource_id). \
+                  filter_by(user=user, permission=permission).first()
+        if row:
+            row.last_updated = timestamp
+        else:
+            obj = UsersScripts(user=user,
+                               resource_id=resource_id,
+                               title=title,
+                               last_updated=timestamp,
+                               permission=permission,
+                               folder=folder)
+            session.add(obj)
+        last_time = timestamp
+    session.commit()
+    return last_time
+
+def fetch_all_users():
+    fetch_by_timestamps('Users', User, 'firstUse', commit_users)
+
+def fetch_all_openid2():
+    fetch_by_timestamps('OpenID2', OpenIDData2, 'timestamp',
+                        commit_open_id_data2)
+
+def fetch_all_users_scripts():
+    fetch_by_timestamps('UsersScripts', UsersScripts, 'last_updated',
+                        commit_users_scripts, USERS_PER_REQUEST=500)
+
+def fetch_all_duplicate_scripts():
+    print "Fetching DuplicateScripts"
+    params = {'table': 'DuplicateScripts'}
+    data = fetch(params)
+    session = Session()
+    for line in data.split('\n'):
+        if line == '':
+            continue
+        __key__, from_script, new_script, from_version = line.split(',')
+        from_version = int(from_version)
+        exists = session.query(DuplicateScript). \
+                     filter_by(new_script=new_script).first()
+        if exists:
+            continue
+        dup = DuplicateScript(from_script=from_script,
+                              new_script=new_script,
+                              from_version=from_version,
+                              __key__=__key__)
+        session.add(dup)
+    session.commit()
+    session.close()
+
+def fetch_all_folders():
+    print "Fetching folders"
+    params = {'table': 'Folders'}
+    data = fetch(params)
+    lines = json.loads(data)
+    session = Session()
+    for  __key__, user, data in lines:
+        obj = session.query(Folder). \
+                     filter_by(user=user).first()
+        if not obj:
+            obj = Folder(user=user, data=data, __key__=__key__)
+            session.add(obj)
+        obj.data = data
+    session.commit()
+    session.close()
+
+def fetch_all_script_data():
+    fetch_by_timestamps('ScriptData', ScriptData, 'timestamp',
+                        commit_script_data)
+
+def fetch_by_timestamps(table, model, timestamp_field, parsing_func, USERS_PER_REQUEST=40):
+    global START_TIME
+    session = Session()
+    all_found = False
+    while not all_found:
+        if START_TIME is None:
+            start = "2000-01-01T01:00:00.000000"
+            timestamp = getattr(model, timestamp_field)
+            last_user = session.query(model).order_by(desc(timestamp)).first()
+            if last_user is not None:
+                start = getattr(last_user, timestamp_field).isoformat()
+            START_TIME = start
+        print "Fetching", table, "starting from:", START_TIME
+        params = {'table': table,
+                  'start_time': START_TIME,
+                  'limit': USERS_PER_REQUEST
+        }
+        data = fetch(params)
+        if len(data) == 0:
+            break
+        last_time = parsing_func(data, session)
+        if last_time is None:
+            break
+        START_TIME = last_time.isoformat()
+        time.sleep(1)
+    print "Completed fetching", table
+
+
+fetch_all_users()
+START_TIME = None
+fetch_all_openid2()
+START_TIME = None
+fetch_all_users_scripts()
+START_TIME = None
+fetch_all_duplicate_scripts()
+START_TIME = None
+fetch_all_script_data()
+fetch_all_folders()
