9b202271aeb6d79f722f0901a9e7375aa31cb713
ritchiew <rawilson52@gmail.com>
2015-04-09 16:38:59 -0400

    try setting up email in staging

--- flask_config.py
+++ flask_config.py
@@ -17,6 +17,12 @@ class StagingConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'dummy')
 
+    MAIL_USERNAME = 'noreply@rawscripts.com'
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'password')
+    MAIL_DEFAULT_SENDER = '"Rawscripts" <noreply@rawscripts.com>'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
