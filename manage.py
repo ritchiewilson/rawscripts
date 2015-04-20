e282b66f865795333a29c8849e5e3b1b9fe6b396
ritchiew <rawilson52@gmail.com>
2015-04-20 19:39:31 -0400

    delete some commands not being used anymore

--- manage.py
+++ manage.py
@@ -37,50 +37,6 @@ def thin_screenplays():
         print "did screenplay:", n, resource_id
     print "Done"
 
-def verify_screenplay(resource_id):
-    if resource_id == 'Demo':
-        return
-    start_from = 1
-    check = MigrationCheck.query.filter_by(resource_id=resource_id).first()
-    if check:
-        start_from = check.verified_to
-    elif DuplicateScript.has_parent(resource_id):
-        dup = DuplicateScript.query.filter_by(new_script=resource_id).first()
-        start_from = max(start_from, dup.from_version + 1)
-    last_save = ResourceVersion.get_latest_version(resource_id)
-    if not last_save:
-        print "ERROR: Not migrated:", resource_id
-        return
-    end_at = last_save.version
-    if ScriptData.has_duplicate_versions(resource_id, start_from, end_at):
-        print "ERROR: Script has multiple saves with same version number:", resource_id
-        return
-
-    last_verified = None
-
-    for version in range(start_from, end_at + 1):
-        if ScriptData.verify_screenplay_at_version(resource_id, version):
-            last_verified = version
-        else:
-            print "Error checking version:", resource_id, version
-            return
-    if last_verified is None:
-        return
-    check = MigrationCheck.query.filter_by(resource_id=resource_id).first()
-    if not check:
-        check = MigrationCheck(resource_id=resource_id)
-        db.session.add(check)
-    check.verified_to = last_verified
-    db.session.commit()
-
-@manager.command
-def verify_screenplays():
-    # resource_ids = UsersScripts.get_all_resource_ids()
-    resource_ids = get_resource_ids()
-    for resource_id in resource_ids:
-        verify_screenplay(resource_id)
-    print "Done"
-
 def migrate_screenplay(resource_id):
     if resource_id == 'Demo':
         return False
@@ -118,76 +74,6 @@ def migrate_to_ops():
         if success:
             ScriptData.thin_raw_data(resource_id)
     print "Done"
-
-@manager.command
-def get_all_email_addresses():
-    num_good = 0
-    tot = 0
-    all_emails = set([])
-    from validate_email import validate_email
-    for user in User.query.all():
-        if not validate_email(user.name) or "http" in user.name:
-            print "failed name: '" + user.name + "'"
-        else:
-            num_good += 1
-        if user.name != user.name.lower():
-            print "Bad case", user.name
-        tot +=1
-    print "Number good-ish:", num_good
-    print "out of", tot
-
-def concatenate(filenames, outfile_name):
-    if os.path.exists(outfile_name):
-        os.remove(outfile_name)
-    with open(outfile_name, 'w') as outfile:
-        for fname in filenames:
-            with open(fname) as infile:
-                for line in infile:
-                    outfile.write(line)
-
-def compile_js(page):
-    all_pages = ['editor', 'scriptlist', 'titlepage']
-    if page not in all_pages:
-        return
-    my_files = "static/js/restricted/{0}/*.js".format(page)
-    temp_file = "static/js/restricted/{0}-temp.js".format(page)
-    concatenate(glob.glob(my_files), temp_file)
-    import closure
-    import subprocess
-    jar = closure.get_jar_filename()
-    min_path = "static/js/min/{0}-compiled.js".format(page)
-    closure_library = "static/closure-library/"
-    calcdeps = closure_library + "/closure/bin/calcdeps.py"
-    with open(min_path, 'w') as f:
-        subprocess.call(["python", calcdeps, "-i", temp_file, "-p", closure_library, "-o", "compiled", "-c", jar, "-f", "--compilation_level=ADVANCED_OPTIMIZATIONS"], stdout=f)
-    os.remove(temp_file)
-
-def compile_css(page):
-    if page not in ['editor', 'scriptlist']:
-        return
-    closure_css = ['menu', 'menuitem', 'menuseparator', 'common', 'toolbar',
-                   'button', 'custombutton', 'autocomplete']
-    if page == "editor":
-        closure_css += ['dialog', 'tab', 'tabbar', 'colormenubutton',
-                        'palette', 'colorpalette', 'editor/bubble',
-                        'editor/dialog', 'editortoolbar']
-    closure_library = "static/closure-library/"
-    path = closure_library + "/closure/goog/css/{0}.css"
-    fnames = [path.format(f) for f in closure_css]
-    fnames.append("static/css/{0}.css".format(page))
-    if not os.path.exists('static/css/min'):
-        os.mkdir('static/css/min')
-    concatenate(fnames, "static/css/min/{0}-all.css".format(page))
-
-@manager.command
-def compile_assets(asset_type, page):
-    all_pages = ['editor', 'scriptlist', 'titlepage']
-    if page not in all_pages and not page == 'all':
-        return
-    pages = all_pages if page == 'all' else [page]
-    compile_func = compile_js if asset_type == 'js' else compile_css
-    for p in pages:
-        compile_func(p)
 
 @manager.command
 def _delete_duplicate_versions(resource_id, version):
