151d32987015af478d8b676aaf78922afe39a54c
ritchiew <rawilson52@gmail.com>
2015-05-01 22:25:26 -0400

    added flask-admin with views for stats and users

--- flask_stats.py
+++ flask_stats.py
@@ -20,26 +20,45 @@ from flask_user import login_required, current_user
 
 from rawscripts import db, app
 from flask_models import User, UsersScripts
+from flask_admin import Admin, BaseView, expose, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
 
 
-@app.route('/stats')
-@login_required
-def stats():
-    if current_user.email != 'rawilson52@gmail.com':
-        abort(401)
-        return
-    query = User.query.with_entities(db.extract('year', User.firstUse),
-                                     db.extract('month', User.firstUse),
-                                     db.func.count()). \
-                group_by(db.extract('year', User.firstUse),
-                         db.extract('month', User.firstUse)). \
-                order_by(db.extract('year', User.firstUse),
-                         db.extract('month', User.firstUse))
-    months = []
-    users = 0
-    for row in query.all():
-        year, month, count = row
-        months.append({'year': year, 'month': month, 'count':count})
-        users += count
-    scripts = db.session.query(db.func.count(db.distinct(UsersScripts.resource_id))).first()[0]
-    return render_template('stats.html', months=months, users=users, scripts=scripts)
+class StatsView(AdminIndexView):
+
+    def is_accessible(self):
+        return current_user.email == 'rawilson52@gmail.com'
+
+    @expose('/')
+    def index(self):
+        query = User.query.with_entities(db.extract('year', User.firstUse),
+                                         db.extract('month', User.firstUse),
+                                         db.func.count()). \
+                    group_by(db.extract('year', User.firstUse),
+                             db.extract('month', User.firstUse)). \
+                    order_by(db.extract('year', User.firstUse),
+                             db.extract('month', User.firstUse))
+        months = []
+        users = 0
+        for row in query.all():
+            year, month, count = row
+            months.append({'year': year, 'month': month, 'count':count})
+            users += count
+        scripts = db.session.query(db.func.count(db.distinct(UsersScripts.resource_id))).first()[0]
+        return self.render('stats.html', months=months, users=users, scripts=scripts)
+
+
+class MyUsersView(ModelView):
+    can_create = False
+    column_searchable_list= ('email',)
+    form_excluded_columns = ('reset_password_token', 'password', 'appengine_user')
+
+    def __init__(self, session, **kwargs):
+        super(MyUsersView, self).__init__(User, session, **kwargs)
+
+    def is_accessible(self):
+        return current_user.email == 'rawilson52@gmail.com'
+
+
+admin = Admin(app, index_view=StatsView())
+admin.add_view(MyUsersView(db.session))
