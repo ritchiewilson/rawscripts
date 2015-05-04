0a2d862cf1a313818d4f66fc08f3279784ee3771
ritchiew <rawilson52@gmail.com>
2015-05-04 15:49:50 -0400

    allow for loading and compiling with jquery

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -14,16 +14,3 @@ mkdir compiler && cd compiler
 wget http://closure-compiler.googlecode.com/files/compiler-latest.zip
 unzip *.zip
 rm *.zip
-
-# Closure library
-cd $ROOT"/static"
-svn checkout http://closure-library.googlecode.com/svn/trunk/ closure-library
-
-#Gdata
-cd $ROOT
-wget http://gdata-python-client.googlecode.com/files/gdata-2.0.14.tar.gz
-tar xvzf gdata-2.0.14.tar.gz
-mv gdata-2.0.14/src/gdata gdata
-mv gdata-2.0.14/src/atom atom
-rm gdata-2.0.14.tar.gz
-rm -rf gdata-2.0.14
